4b84f45ec0263bbff02dbd5fbc0302512a816e17
Zarrina Niyazova <zarrina_@hotmail.com>
2018-02-02 12:17:29 -0500

    corrects indentation

--- index.js
+++ index.js
@@ -18,41 +18,43 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 function getFilmRecommendations(req, res) {
 //  res.status(500).send('Not Implemented');
 
-const sequelize = new Sequelize('null', 'null', 'null', {
-  dialect: 'sqlite',
-  storage: 'db/database.db',
-  define: {
-    timestamps: false
-  }
-});
+  const sequelize = new Sequelize('null', 'null', 'null', {
+    dialect: 'sqlite',
+    storage: 'db/database.db',
+    define: {
+      timestamps: false
+    }
+  });
 
-//MODELS
-const genres = sequelize.define('genres', {
-  id: { allowNull: false, primaryKey: true, unique: true, type: Sequelize.INTEGER },
-  name: { allowNull: false, type: Sequelize.STRING }
-});
+  //MODELS
+  const genres = sequelize.define('genres', {
+    id: { allowNull: false, primaryKey: true, unique: true, type: Sequelize.INTEGER },
+    name: { allowNull: false, type: Sequelize.STRING }
+  });
 
-const films = sequelize.define('films', {
-  id: { allowNull: false, primaryKey: true, unique: true, type: Sequelize.INTEGER },
-  title: { allowNull: false, type: Sequelize.STRING },
-  release_date: { allowNull: false, type: Sequelize.DATE },
-  tagline: { allowNull: false, type: Sequelize.STRING },
-  revenue: { allowNull: false, defaultValue: 0, type: Sequelize.BIGINT },
-  budget: { allowNull: false, type: Sequelize.BIGINT },
-  runtime: { allowNull: false, type: Sequelize.INTEGER },
-  original_language: { allowNull: false, type: Sequelize.STRING },
-  status: { allowNull: false, type: Sequelize.STRING },
-  genre_id: { allowNull: false, type: Sequelize.INTEGER }
-});
+  const films = sequelize.define('films', {
+    id: { allowNull: false, primaryKey: true, unique: true, type: Sequelize.INTEGER },
+    title: { allowNull: false, type: Sequelize.STRING },
+    release_date: { allowNull: false, type: Sequelize.DATE },
+    tagline: { allowNull: false, type: Sequelize.STRING },
+    revenue: { allowNull: false, defaultValue: 0, type: Sequelize.BIGINT },
+    budget: { allowNull: false, type: Sequelize.BIGINT },
+    runtime: { allowNull: false, type: Sequelize.INTEGER },
+    original_language: { allowNull: false, type: Sequelize.STRING },
+    status: { allowNull: false, type: Sequelize.STRING },
+    genre_id: { allowNull: false, type: Sequelize.INTEGER }
+  });
 
-//RELATIONS
-genres.hasOne(films, {
-  foreignKey: 'genre_id'
-});
+  //RELATIONS
+  genres.hasOne(films, {
+    foreignKey: 'genre_id'
+  });
 
-films.belongTo(genres, {
-  foreignKey: 'genre_id'
-});
+  films.belongTo(genres, {
+    foreignKey: 'genre_id'
+  });
+
+
 
 
 
